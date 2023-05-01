a8ad8bcc97602868e78e45c0d6f47f5442f672ff
addisu30 <dabaleaddisu21@outlook.com>
2023-05-01 23:58:18 +0300

    places review updating

---
+++ api/v1/views/places_reviews.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Contains the places_reviews view for the API.'''
+from flask import abort, jsonify, make_response, request
+from api.v1.views import app_views
+from models import storage
+from models.place import Place
+from models.review import Review
+from models.user import User
