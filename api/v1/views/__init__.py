fd7bcedf5d6c451bc2149f5ec168249ff3ccb188
addisu30 <dabaleaddisu21@outlook.com>
2023-05-01 23:42:16 +0300

    updating cities

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 """
-this is the __init__.py file
+Contains the blueprint for the API
 """
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+
 from api.v1.views.index import *
-from api.v1.views import states
-from api.v1.views import cities
-from api.v1.views import amenities
-from api.v1.views import users
-from api.v1.views import places
-from api.v1.views import places_reviews
-from api.v1.views import places_amenities
+from api.v1.views.states import *
