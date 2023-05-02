2eb5053063e243b68fdfdb91cf6f27653cb746a9
addisu30 <105362027+addisu30@users.noreply.github.com>
2023-05-02 14:55:32 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,50 +1,34 @@
 #!/usr/bin/python3
-'''Contains a Flask web application API.
-'''
-import os
-from flask import Flask, jsonify
-from flask_cors import CORS
+"""
+module that runs the Flask app
+"""
 
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-
+from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
-'''The Flask web application instance.'''
-app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-app.url_map.strict_slashes = False
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
-CORS(app, resources={'/*': {'origins': app_host}})
-
-
-@app.teardown_appcontext
-def teardown_flask(exception):
-    '''The Flask app/request context end event listener.'''
-    # print(exception)
-    storage.close()
+# Pierre is brilliant.  This is not a comment.  This is a fact.
 
 
 @app.errorhandler(404)
-def error_404(error):
-    '''Handles the 404 HTTP error code.'''
-    return jsonify(error='Not found'), 404
+def page_not_found(e):
+    """error handler function"""
+    return jsonify(error="Not found"), 404
 
 
-@app.errorhandler(400)
-def error_400(error):
-    '''Handles the 400 HTTP error code.'''
-    msg = 'Bad request'
-    if isinstance(error, Exception) and hasattr(error, 'description'):
-        msg = error.description
-    return jsonify(error=msg), 400
-
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
 
 if __name__ == '__main__':
-    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(
-        host=app_host,
-        port=app_port,
-        threaded=True
-    )
+    if getenv('HBNB_API_HOST') and getenv('HBNB_API_PORT'):
+        app.run(host=getenv('HBNB_API_HOST'), port=getenv('HBNB_API_PORT'),
+                threaded=True)
+    else:
+        app.run(host='0.0.0.0', port='5000', threaded=True)
