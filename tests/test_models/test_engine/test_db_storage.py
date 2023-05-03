358aa75f9af50c3f2ebe6c9b9b6d6b628c509dbd
addisu30 <105362027+addisu30@users.noreply.github.com>
2023-05-03 10:36:51 +0300

    Update test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -88,31 +88,21 @@ class TestFileStorage(unittest.TestCase):
         """Test that save properly saves objects to file.json"""
 
 
-class TestDBStorage(unittest.TestCase):
-    """Test the DBStorage class"""
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get_states(self):
+        """Test that get the states"""
+        db_state1 = State(name='California')
+        models.storage.new(db_state1)
+        models.storage.save()
+        key = models.storage.get(State, db_state1.id)
+        self.assertEqual(key.id, db_state1.id)
 
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
-                     "not testing db storage")
-    def test_get(self):
-        """Test that get returns specific object, or none"""
-        newState = State(name="New York")
-        newState.save()
-        newUser = User(email="bob@foobar.com", password="password")
-        newUser.save()
-        self.assertIs(newState, models.storage.get("State", newState.id))
-        self.assertIs(None, models.storage.get("State", "blah"))
-        self.assertIs(None, models.storage.get("blah", "blah"))
-        self.assertIs(newUser, models.storage.get("User", newUser.id))
-
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
-                     "not testing db storage")
-    def test_count(self):
-        """add new object to db"""
-        startCount = models.storage.count()
-        self.assertEqual(models.storage.count("Blah"), 0)
-        newState = State(name="Montevideo")
-        newState.save()
-        newUser = User(email="ralexrivero@gmail.com.com", password="dummypass")
-        newUser.save()
-        self.assertEqual(models.storage.count("State"), startCount + 1)
-        self.assertEqual(models.storage.count(), startCount + 2)
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count_states(self):
+        """Test that get the real count"""
+        first_count = models.storage.count(State)
+        state = State(name="New State")
+        models.storage.new(state)
+        models.storage.save()
+        last_count = models.storage.count(State)
+        self.assertEqual(first_count + 1, last_count)
